b93ddf3f40c1b8b39bfb3f51f52c124f2cb6dbeb
coderman1018 <96354459+coderman1018@users.noreply.github.com>
2023-12-09 10:42:07 +0000

    replit code

--- main.py
+++ main.py
@@ -9,9 +9,6 @@ from datetime import date
 import datetime
 import ctypes
 
-
-
-
 app = Flask(__name__)
 app.secret_key = "sociopact-123"
 ckeditor = CKEditor(app)
@@ -23,17 +20,19 @@ db = SQLAlchemy(app)
 
 today = datetime.datetime.today().weekday()
 
+
 class CreatePostForm(FlaskForm):
     title = StringField("Task Title:", validators=[DataRequired()])
     body = CKEditorField("Task Content:", validators=[DataRequired()])
     submit = SubmitField("Submit Task")
+
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(250), unique=False, nullable=False)
     password = db.Column(db.String(250), unique=False, nullable=False)
     email = db.Column(db.String(100), unique=False, nullable=False)
-    address = db.Column(db.String(100), unique=False, nullable=False) 
+    address = db.Column(db.String(100), unique=False, nullable=False)
     level = db.Column(db.Integer, unique=False, nullable=True)
     totalpoints = db.Column(db.Integer, unique=False, nullable=True)
     helped = db.Column(db.Integer, unique=False, nullable=True)
@@ -41,6 +40,7 @@ class User(db.Model):
     tocomplete = db.Column(db.String(950), nullable=True)
     ratings = db.Column(db.String(950), nullable=True)
     volunteered = db.Column(db.String(950), nullable=True)
+
 
 class BlogPost(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -55,14 +55,11 @@ class BlogPost(db.Model):
     accepted = db.Column(db.String(950), nullable=False)
     doneby = db.Column(db.String(950), nullable=False)
     donebydate = db.Column(db.String(250), nullable=True)
-    
-
-
-
 
 
 with app.app_context():
     db.create_all()
+
 
 @app.route("/signup", methods=["GET", "POST"])
 def signup():
@@ -70,30 +67,27 @@ def signup():
         email = request.form["email"]
         dup = False
         for x in User.query.all():
-            if x.email==email: 
+            if x.email == email:
                 dup = True
         if not dup:
             new_user = User(username=request.form["username"],
                             password=request.form["password"],
-                            email = request.form["email"],
-                            address = request.form["neighborhoods"],
+                            email=request.form["email"],
+                            address=request.form["neighborhoods"],
                             level=1,
                             totalpoints=0,
                             helped=0,
                             completed='',
                             tocomplete='',
                             ratings='',
-                            volunteered=''
-                                            )
+                            volunteered='')
             db.session.add(new_user)
             db.session.commit()
 
             return redirect(url_for('login'))
-        else:
-            MessageBox = ctypes.windll.user32.MessageBoxW
-            MessageBox(None, 'You have already signed up with this email. Try logging in instead', 'Duplicate Email', "0x10000")
-            # win32api.MessageBox(0, 'You have already signed up with this email. Try logging in instead', 'Duplicate Email',4096)
+
     return render_template("signup.html")
+
 
 @app.route('/login', methods=["GET", "POST"])
 def login():
@@ -102,61 +96,66 @@ def login():
         userpass = request.form["password"]
         myuser = User.query.filter_by(email=selected).first()
 
-        if not myuser or myuser.password!=userpass:
-            MessageBox = ctypes.windll.user32.MessageBoxW
-            MessageBox(None, 'Incorrect username or password', 'Error', "0x10000")
-            # win32api.MessageBox(0, 'Incorrect username or password', 'Error',4096)
-            
-        else:
+        # if not myuser or myuser.password!=userpass:
+        #     MessageBox = ctypes.windll.user32.MessageBoxW
+        #     MessageBox(None, 'Incorrect username or password', 'Error', "0x10000")
+        #     # win32api.MessageBox(0, 'Incorrect username or password', 'Error',4096)
+
+        if myuser:
 
             session['username'] = myuser.username
             session['level'] = myuser.level
             session['totalpoints'] = myuser.totalpoints
             session['helped'] = myuser.helped
-            
 
             return redirect(url_for("index"))
     return render_template("login.html")
+
 
 @app.route('/')
 def notuser():
     return render_template("notuser.html")
 
+
 @app.route('/index')
 def index():
-    current = User.query.filter_by(username=session.get('username', None)).first()
+    current = User.query.filter_by(
+        username=session.get('username', None)).first()
     d = {}
-    if current.tocomplete!='':
+    if current.tocomplete != '':
         mylist = current.tocomplete.split('+')
         for x in mylist:
-            if x!='':
+            if x != '':
                 post = BlogPost.query.filter_by(title=x).first()
-                d[x] = [post.author,User.query.filter_by(username=post.author).first().email,post.id]
-                
+                d[x] = [
+                    post.author,
+                    User.query.filter_by(username=post.author).first().email,
+                    post.id
+                ]
 
     d2 = {}
-    if current.completed!='':
+    if current.completed != '':
         mylist = current.completed.split('+')
         for x in mylist:
-            if x!='':
+            if x != '':
                 post = BlogPost.query.filter_by(title=x).first()
-                d2[x]=post.author
+                d2[x] = post.author
     d3 = {}
-    if current.volunteered!='':
+    if current.volunteered != '':
         mylist = current.volunteered.split('+')
         for x in mylist:
-            if x!='':
+            if x != '':
                 post = BlogPost.query.filter_by(title=x).first()
-                d3[x]=[post.author,post.archived,post.doneby,post.id]
+                d3[x] = [post.author, post.archived, post.doneby, post.id]
 
     return render_template("index.html",
                            user=current.username,
                            level=current.level,
                            totalpoints=current.totalpoints,
-                           completed = d2,
-                           tocomplete = d,
-                           volunteeredfor = d3
-                           )
+                           completed=d2,
+                           tocomplete=d,
+                           volunteeredfor=d3)
+
 
 @app.route("/delete/<int:post_id>")
 def delete_post(post_id):
@@ -165,17 +164,18 @@ def delete_post(post_id):
     db.session.commit()
     return redirect(url_for('myposts'))
 
+
 @app.route("/volunteer/<int:post_id>")
 def volunteer(post_id):
     volunteering = BlogPost.query.filter_by(id=post_id).first()
-    person = User.query.filter_by(username=session.get('username', None)).first()
-    person.volunteered+=volunteering.title+'+'
+    person = User.query.filter_by(
+        username=session.get('username', None)).first()
+    person.volunteered += volunteering.title + '+'
     if person.username not in volunteering.people:
-        volunteering.response+=1
-        volunteering.people+=person.username+' '
+        volunteering.response += 1
+        volunteering.people += person.username + ' '
         db.session.commit()
     return redirect(url_for('tasks'))
-
 
 
 @app.route("/unarchive/<int:post_id>")
@@ -185,18 +185,19 @@ def unarchive(post_id):
     db.session.commit()
     return redirect(url_for('myposts'))
 
+
 @app.route('/rating/<name>/<title>', methods=["GET", "POST"])
-def rating(name,title):
+def rating(name, title):
     if request.method == "POST":
         num = int(request.form.get("stars"))
         helper = User.query.filter_by(username=name).first()
-        helper.ratings+=str(num)+' '
-        helper.totalpoints+=num*2
-        helper.helped+=1
+        helper.ratings += str(num) + ' '
+        helper.totalpoints += num * 2
+        helper.helped += 1
 
         post = BlogPost.query.filter_by(title=title).first()
-        post.doneby=helper.username
-        post.archived=1
+        post.doneby = helper.username
+        post.archived = 1
         post.donebydate = date.today().strftime("%B %d, %Y")
         mylist = post.accepted.split()
         myindex = mylist.index(helper.username)
@@ -208,67 +209,71 @@ def rating(name,title):
         popped = mylist.pop(myindex)
         helper.tocomplete = '+'.join(mylist)
 
-        helper.completed+=popped+'+'
+        helper.completed += popped + '+'
 
         db.session.commit()
         return redirect(url_for('myposts'))
-    return render_template("rating.html",name=name, title=title)
+    return render_template("rating.html", name=name, title=title)
+
 
 @app.route('/complete/<person>/<title>')
-def complete(person,title):
+def complete(person, title):
     user = User.query.filter_by(username=person).first()
     user.completed.append(title)
     db.session.commit()
     return redirect(url_for('index'))
 
+
 @app.route('/accepted/<title>/<person>')
-def accepted(title,person):
+def accepted(title, person):
     myuser = User.query.filter_by(username=person).first()
     post = BlogPost.query.filter_by(title=title).first()
     mylist = post.people.split()
     myindex = mylist.index(person)
     one = mylist.pop(myindex)
     post.people = ' '.join(mylist)
-    post.response-=1
-    post.accepted += one+' '
-    myuser.tocomplete+=title+'+'
+    post.response -= 1
+    post.accepted += one + ' '
+    myuser.tocomplete += title + '+'
     db.session.commit()
     return redirect(url_for('myposts'))
 
+
 @app.route('/deny/<title>/<person>')
-def deny(title,person):
+def deny(title, person):
     post = BlogPost.query.filter_by(title=title).first()
     mylist = post.people.split()
     myindex = mylist.index(person)
     mylist.pop(myindex)
     post.people = ' '.join(mylist)
-    post.response-=1
+    post.response -= 1
     db.session.commit()
     return redirect(url_for('myposts'))
 
 
 @app.route("/about")
 def about():
-    current = User.query.filter_by(username=session.get('username', None)).first()
-    if current.totalpoints>=100:
-        current.level+=1
-        current.totalpoints-=100
-    avg=[]
+    current = User.query.filter_by(
+        username=session.get('username', None)).first()
+    if current.totalpoints >= 100:
+        current.level += 1
+        current.totalpoints -= 100
+    avg = []
     for i in current.ratings.split():
         avg.append(int(i))
-    average = round(sum(avg)/len(avg),1) if avg!=[] else 0
-    users = [x for x in User.query.all() if current.address==x.address]
-    details=[]
+    average = round(sum(avg) / len(avg), 1) if avg != [] else 0
+    users = [x for x in User.query.all() if current.address == x.address]
+    details = []
     for x in users:
-        details.append([x.username,(x.level-1)*100+x.totalpoints])
-    details = sorted(details,key=lambda x:x[1])[::-1]
-    leaderboard=True
-    if len(details)<3:
-        one,two,three = 0,0,0
-        leaderboard=False
-    if len(details)>=3:
-        one,two,three = details[0],details[1],details[2]
-    if today==6 and one[1]!=0 and one[0]==current.username:
+        details.append([x.username, (x.level - 1) * 100 + x.totalpoints])
+    details = sorted(details, key=lambda x: x[1])[::-1]
+    leaderboard = True
+    if len(details) < 3:
+        one, two, three = 0, 0, 0
+        leaderboard = False
+    if len(details) >= 3:
+        one, two, three = details[0], details[1], details[2]
+    if today == 6 and one[1] != 0 and one[0] == current.username:
         winner = True
     else:
         winner = False
@@ -276,35 +281,45 @@ def about():
                            user=current.username,
                            level=current.level,
                            totalpoints=current.totalpoints,
-                           helped = current.helped,
+                           helped=current.helped,
                            average=average,
                            one=one,
                            two=two,
                            three=three,
                            leaderboard=leaderboard,
-                           winner=winner
-                           )
+                           winner=winner)
 
 
 @app.route("/tasks")
 def tasks():
     posts = BlogPost.query.all()
-    current = User.query.filter_by(username=session.get('username', None)).first()
-    return render_template("blog.html", all_posts=posts[::-1], now=current.username,now2=current.address)
+    current = User.query.filter_by(
+        username=session.get('username', None)).first()
+    return render_template("blog.html",
+                           all_posts=posts[::-1],
+                           now=current.username,
+                           now2=current.address)
+
 
 @app.route("/selection/<author>")
 def selection(author):
-    current = User.query.filter_by(username=session.get('username', None)).first()
+    current = User.query.filter_by(
+        username=session.get('username', None)).first()
     posts = BlogPost.query.all()
     selected = []
     for x in posts:
-        if x.author==author and x.archived==0:
+        if x.author == author and x.archived == 0:
             selected.append(x)
-    return render_template("selection.html", author=author,selected=selected[::-1],now=current.username)
-    
+    return render_template("selection.html",
+                           author=author,
+                           selected=selected[::-1],
+                           now=current.username)
+
+
 @app.route("/myposts")
 def myposts():
-    current = User.query.filter_by(username=session.get('username', None)).first()
+    current = User.query.filter_by(
+        username=session.get('username', None)).first()
     posts = BlogPost.query.all()
     details = {}
     people_details = {}
@@ -315,58 +330,62 @@ def myposts():
         details[x.title] = everyone
         for selected in everyone:
             myuser = User.query.filter_by(username=selected).first()
-            level,points = myuser.level,myuser.totalpoints
+            level, points = myuser.level, myuser.totalpoints
             for i in myuser.ratings.split():
                 avg.append(int(i))
-            average = round(sum(avg)/len(avg),1) if avg!=[] else 0
-            people_details[selected] = [level,points,average]
-            
-        accept[x.title]=x.accepted
-    return render_template("myposts.html", all_posts=posts, now=current.username, details=details, everyone=people_details, clicked=accept)
+            average = round(sum(avg) / len(avg), 1) if avg != [] else 0
+            people_details[selected] = [level, points, average]
+
+        accept[x.title] = x.accepted
+    return render_template("myposts.html",
+                           all_posts=posts,
+                           now=current.username,
+                           details=details,
+                           everyone=people_details,
+                           clicked=accept)
+
 
 @app.route("/new-post", methods=["GET", "POST"])
 def new_post():
-    current = User.query.filter_by(username=session.get('username', None)).first()
+    current = User.query.filter_by(
+        username=session.get('username', None)).first()
     form = CreatePostForm()
     if form.validate_on_submit():
-        new_post = BlogPost(
-            title=form.title.data,
-            body=form.body.data,
-            author=current.username,
-            date=date.today().strftime("%B %d, %Y"),
-            address=current.address,
-            response = 0,
-            archived = 0,
-            people = '',
-            accepted = '',
-            doneby='',
-            donebydate = ''
-        )
+        new_post = BlogPost(title=form.title.data,
+                            body=form.body.data,
+                            author=current.username,
+                            date=date.today().strftime("%B %d, %Y"),
+                            address=current.address,
+                            response=0,
+                            archived=0,
+                            people='',
+                            accepted='',
+                            doneby='',
+                            donebydate='')
         db.session.add(new_post)
         db.session.commit()
         return redirect(url_for("tasks"))
     return render_template("add.html", form=form)
 
+
 @app.route("/edit-post/<int:post_id>", methods=["GET", "POST"])
 def edit_post(post_id):
     post = BlogPost.query.get(post_id)
-    edit_form = CreatePostForm(
-        title=post.title,
-        body=post.body
-    )
+    edit_form = CreatePostForm(title=post.title, body=post.body)
     if edit_form.validate_on_submit():
         post.title = edit_form.title.data
-        post.body = edit_form.body.data    
+        post.body = edit_form.body.data
         db.session.commit()
         return redirect(url_for("myposts"))
     return render_template("add.html", form=edit_form, is_edit=True)
 
+
 @app.route("/onepost/<int:post_id>/<ishome>")
-def onepost(post_id,ishome):
+def onepost(post_id, ishome):
     print(ishome)
     post = BlogPost.query.get(post_id)
-    return render_template("onepost.html",post=post,ishome=ishome)
+    return render_template("onepost.html", post=post, ishome=ishome)
+
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=81)
-
